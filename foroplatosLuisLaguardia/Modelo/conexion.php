e63b1f57f96e9a96a0c8627b69fbc839c2fe0b40
LuisLaguardia31 <22.dam.luislaguardia@fundacionmontessori.com>
2024-12-16 17:55:55 +0100

    Update3 conexion.php

--- foroplatosLuisLaguardia/Modelo/conexion.php
+++ foroplatosLuisLaguardia/Modelo/conexion.php
@@ -2,7 +2,6 @@
 try {
     $pdo = new PDO("mysql:host=localhost;dbname=foroplatosLuisLaguardia", "admin", "admin");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Conexión exitosa a la base de datos!";
 } catch (PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
